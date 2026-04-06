77d0efd33308327b8493c65df45aae4fe2bdcaf0
Fatima-Rahmani79 <fatima.rahmani79@gmail.com>
2026-04-06 10:13:31 +0430

    respnonsive teachers.html page

--- assets/js/teachers.js
+++ assets/js/teachers.js
@@ -224,8 +224,8 @@ function renderTeachers() {
       const shortBio = teacher.bio.split(".")[0] + ".";
 
       return `
-        <div class="col-lg-4 col-md-6 col-sm-12">
-            <div class="card course-card h-100" style="min-width: auto; max-width: 100%;">
+        <div class="col-lg-4 col-md-6 col-sm-12 d-flex justify-content-center card-width">
+            <div class="card course-card h-100" style="width: 100%; max-width: 350px;">
                 <div class="position-relative">
                     <img src="${teacher.image}" class="card-img-top" alt="${teacher.name}" style="height: 250px; object-fit: cover;">
                     <div class="position-absolute top-0 start-0 m-2">
